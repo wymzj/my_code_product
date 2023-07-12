3df34311cf7e0f742eef7dc664d5fe445f6bd4e2
wymzj <106292211+wymzj@users.noreply.github.com>
2023-07-12 09:26:43 +0800

    Add files via upload

---
+++ 1.2.1/import_test.py
@@ -0,0 +1,6 @@
+import python_import as pi
+pi.package1.module_1.hello()
+def_class = pi.package2.module_class.define_class()
+def_class.method_1()
+pi.package3.module_1.hello()
+pi.package4.module_1.hello()
