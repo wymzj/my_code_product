0e08f1dcb3e19f6436e0ff622c37533067ffabc0
培训课程 <106292211+wymzj@users.noreply.github.com>
2024-05-04 09:54:27 +0800

    Update primer_Lesson_3.cpp

--- primer_Lesson_3.cpp
+++ primer_Lesson_3.cpp
@@ -16,6 +16,7 @@
 #include <string>
 
 #define PI 3.1415926                        //宏定义 
+#undef PI                                   //取消宏定义
 //#define 标识符  字符串序列。宏定义有三个缺点：不能类型检查；没有作用域；不受命名空间限制。
 
 #define Print(var)  cout<<(var)<<endl       //宏置换
